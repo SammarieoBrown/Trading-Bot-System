13a91a842818dc0fa359da6224d1f39d501e43cd
Sammarieo Brown <sammarieobrow21@gmail.com>
2022-10-23 13:05:00 -0500

    alpaca

--- Bot/Backtester Framework/Main.py
+++
@@ -1,9 +0,0 @@
-from Config import Config
-from Bot import Bot
-
-#  Main
-if __name__ == '__main__':
-    #  Create new bot
-    config = Config(symbols=['BTC-USD'], cash_per_trade=100, total_cash=10000, data_fetch_interval=60)
-    bot = Bot(config)
-    bot.start()
